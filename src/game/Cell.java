fce13038a8228b44d7be79659173cded6fba7344
Erik Janeck <Brexten92@yahoo.de>
2017-10-20 15:57:15 +0200

    fusion

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,42 +1,42 @@
 package game;
-
+ 
 public class Cell {
-	protected int positionX;
-	protected int positionY;
-	private boolean alive;
-	
-	public int getPositionX() {
-		return positionX;
-	}
-
-	public void setPositionX(int positionX) {
-		this.positionX = positionX;
-	}
-
-	public int getPositionY() {
-		return positionY;
-	}
-
-	public void setPositionY(int positionY) {
-		this.positionY = positionY;
-	}
-	
-	public Cell(int posX, int posY){
-		this.alive = false;
-		this.positionX = posX;
-		this.positionY = posY;
-	};
-	
-	public void lives(){
-		this.alive = true;
-	}
-	
-	public void dies(){
-		this.alive = false;
-	}
-	
-	public boolean isAlive(){
-		return alive;
-	}
-	
+  protected int positionX;
+  protected int positionY;
+  private boolean alive;
+  
+  public int getPositionX() {
+    return positionX;
+  }
+ 
+  public void setPositionX(int positionX) {
+    this.positionX = positionX;
+  }
+ 
+  public int getPositionY() {
+    return positionY;
+  }
+ 
+  public void setPositionY(int positionY) {
+    this.positionY = positionY;
+  }
+  
+  public Cell(int posX, int posY){
+    this.alive = false;
+    this.positionX = posX;
+    this.positionY = posY;
+  }
+  
+  public void live(){
+    this.alive = true;
+  }
+  
+  public void die(){
+    this.alive = false;
+  }
+  
+  public boolean status(){
+    return alive;
+  }
+  
 }
