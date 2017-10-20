8e0a6b54f1b362f8c1d1f29ce221c389805edf62
Erik Janeck <Brexten92@yahoo.de>
2017-10-20 16:11:13 +0200

    Auto stash before merge of "ECJ" and "origin/ECJ"

---
+++ src/gameModes/GameOfLife1.java
@@ -0,0 +1,10 @@
+package gameModes;
+
+import game.Generation;
+
+public class GameOfLife1 { 
+  public static void main(String[] args) { 
+    Generation newGen = Generation.GenerationConf1(); 
+  } 
+ 
+} 
