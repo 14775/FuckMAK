275996fa2ed5989b36ca2574677935738594930f
14710 <nico.knueppel@nordakademie.de>
2017-10-24 11:57:58 +0200

    added mockito

---
+++ src/test/testGrid.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testGrid {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
