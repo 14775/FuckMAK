8ce653232f99e34135b852574cb041d7a77991ef
14592 <willi.prahl@nordakademie.de>
2017-10-24 11:58:37 +0200

    nochmal irgendwas

--- src/gameModes/GameOfLife1.java
+++ src/gameModes/GameOfLife1.java
@@ -4,7 +4,7 @@ import game.GenerationConf1;
 
 public class GameOfLife1 {
 	public static void main(String[] args) {
-		GenerationConf1 newGen = new GenerationConf1(40, 0, 1);
+		GenerationConf1 newGen = new GenerationConf1(40, 0, 0);
 		for (int i = 1; i <= 50; i++) {
 			newGen.nextGeneration();
 			newGen.drawGrid();
