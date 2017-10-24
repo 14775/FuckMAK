191d1adb4a59ce5dc64a35f768f27cb3f6720bdc
Erik Janeck <Brexten92@yahoo.de>
2017-10-24 17:56:21 +0200

    Broken Deluxe Hit Single II

--- src/gameModes/GameOfLife2.java
+++ src/gameModes/GameOfLife2.java
@@ -5,7 +5,7 @@ import game.GenerationConf2;
 public class GameOfLife2 {
 
 	public static void main(String[] args) {
-		GenerationConf2 newGen = new GenerationConf2(100, "2DArray", 0, "VonNeumann");
+		GenerationConf2 newGen = new GenerationConf2(100, "2DArray", 0, "vonNeumann");
 		for (int i = 0; i <= 50; i++) {
 			newGen.nextGeneration();
 			newGen.drawGrid();
