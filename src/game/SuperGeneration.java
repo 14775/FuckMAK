843b1f3a7f2ed8315dd4123f0fff88b7fb6c9d20
14592 <willi.prahl@nordakademie.de>
2017-10-24 11:09:11 +0200

    Nachbarschaft am Rand abschneiden korrigiert.

--- src/game/SuperGeneration.java
+++ src/game/SuperGeneration.java
@@ -79,7 +79,7 @@ public class SuperGeneration {
 
 	public int getNumberOfAliveNeighbors(List<Number> neighbors) {
 		int numberOfAliveNeighbors = 0;
-		for (int i = 0; i <= (neighbors.size() - 2); i++) {
+		for (int i = 0; i < (neighbors.size() - 2); i++) {
 			// Am Rand abschneiden
 			// TODO korrigieren!
 			if ((int) neighbors.get(i) >= 0 && (int) neighbors.get(i + 1) <= dimension) {
