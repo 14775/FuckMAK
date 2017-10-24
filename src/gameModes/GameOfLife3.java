a10632cf62ca2a8ca0f981e0133e3014d8f27ec8
14592 <willi.prahl@nordakademie.de>
2017-10-24 20:39:46 +0200

    Log Package angelegt

--- src/gameModes/GameOfLife3.java
+++ src/gameModes/GameOfLife3.java
@@ -5,7 +5,7 @@ import game.GenerationConf3;
 public class GameOfLife3 {
 
 	public static void main(String[] args) {
-		GenerationConf3 newGen = new GenerationConf3(300, "2DArray", 0, "vonNeumann");
+		GenerationConf3 newGen = new GenerationConf3(300, "2DArray", 0, "Moore");
 		newGen.nextGeneration();
 		newGen.drawGrid();
 	}
---
+++ src/log/Log.java
@@ -0,0 +1,5 @@
+package log;
+
+public class Log {
+
+}
