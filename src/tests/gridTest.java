683609bbfc1160ac6e19dfe9207d6168727bf53a
14710 <nico.knueppel@nordakademie.de>
2017-10-25 17:26:05 +0200

    test package

---
+++ src/tests/gridTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class gridTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
