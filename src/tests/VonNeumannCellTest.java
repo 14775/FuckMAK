2f8cc3f82dc9d3e72a587c902498f23acb31d5ce
14592 <willi.prahl@nordakademie.de>
2017-10-26 11:44:44 +0200

    cellTests

---
+++ src/tests/VonNeumannCellTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import grid.types.Grid;
+import grid.types.Grid2DArray;
+
+public class VonNeumannCellTest {
+	Grid grid;
+	final int dimension = 10;
+	final int checkpos = 5;
+	final String celltype = "VonNeumann";
+
+	@Before
+	/*
+	 * Creates a Grid
+	 */
+	public void setUp() {
+		grid = new Grid2DArray(dimension, celltype);
+	}
+
+	/*
+	 * tests neighbors() for a cell at [5][5]
+	 */
+	@Test
+	public void neighborsCheck() {
+		assertEquals(dimension, grid.getGridDimension());
+		List<Number> neighbors = grid.getCell(checkpos, checkpos).neighbors();
+		assertEquals(neighbors.get(0), 5);
+		assertEquals(neighbors.get(1), 4);
+		assertEquals(neighbors.get(2), 5);
+		assertEquals(neighbors.get(3), 6);
+		assertEquals(neighbors.get(4), 4);
+		assertEquals(neighbors.get(5), 5);
+		assertEquals(neighbors.get(6), 6);
+		assertEquals(neighbors.get(7), 5);
+	}
+
+}
