11744a6f76348910528f8f22a3c6c8c1cbb5d22a
Jan Keller <jan.keller@nordakademie.de>
2017-10-27 17:47:40 +0200

    Controller implementation for GoL1 + break condition interface + sameGridBreakCondition

--- src/gameModes/GameOfLife1.java
+++ src/gameModes/GameOfLife1.java
@@ -1,12 +1,42 @@
 package gameModes;
 
+import game.Controller;
+
 public class GameOfLife1 {
+	static Controller controller = new Controller();
+
 	public static void main(String[] args) {
-		GameOfLifeConfig1 newGen = new GameOfLifeConfig1(40, "2DArray", 0, "Moore");
-		for (int i = 1; i <= 50; i++) {
-			newGen.nextGeneration();
-			newGen.drawGrid();
-		}
+		controller.createGame(40, "2DArray", 0, "Moore", 0);
+		setStartConfiguration();
+		controller.nextGeneration();
 	}
 
+	private static void setStartConfiguration() {
+		int row;
+		int col;
+		for (row = 18; row <= 23; row++) {
+			for (col = 17; col <= 23; col++) {
+				if (row == 18 || row == 19) {
+					if (col == 18 || col == 19 || col == 21 || col == 22) {
+						controller.getGrid().getCell(row, col).live();
+					}
+				}
+				if (row == 20) {
+					if (col == 19 || col == 21) {
+						controller.getGrid().getCell(row, col).live();
+					}
+				}
+				if (row == 21 || row == 22) {
+					if (col == 17 || col == 19 || col == 21 || col == 23)
+						controller.getGrid().getCell(row, col).live();
+				}
+				if (row == 23) {
+					if (col == 17 || col == 18 || col == 22 || col == 23)
+						controller.getGrid().getCell(row, col).live();
+				}
+
+			}
+		}
+		controller.drawGrid();
+	}
 }
