ddd61dd8063e1d573e4d1be333aa6b7171227ccd
14592 <willi.prahl@nordakademie.de>
2017-10-27 14:12:08 +0200

    Parity Rules Test implementiert

--- src/tests/ParityRulesTest.java
+++ src/tests/ParityRulesTest.java
@@ -18,22 +18,51 @@ public class ParityRulesTest {
 	}
 
 	/*
-	 * tests getCell() and insertCell()
+	 * tests rules by checking the expected cell states after the first run
 	 */
 	@Test
-	public void testParityRules() {
+	public void testParityRules1() {
 
 		gen.getGrid().getCell(1, 2).live();
 		gen.nextGeneration();
-
+		// alive cells after first run
 		assertEquals(true, gen.getGrid().getCell(1, 1).isAlive());
 		assertEquals(true, gen.getGrid().getCell(0, 2).isAlive());
 		assertEquals(true, gen.getGrid().getCell(2, 2).isAlive());
 		assertEquals(true, gen.getGrid().getCell(1, 3).isAlive());
-
+		// dead cells after first run
 		assertEquals(false, gen.getGrid().getCell(0, 0).isAlive());
 		assertEquals(false, gen.getGrid().getCell(0, 1).isAlive());
 		assertEquals(false, gen.getGrid().getCell(0, 3).isAlive());
+		assertEquals(false, gen.getGrid().getCell(1, 0).isAlive());
+		assertEquals(false, gen.getGrid().getCell(2, 0).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 0).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 1).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 2).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 3).isAlive());
+	}
+
+	/*
+	 * tests rules by checking the expected cell states after the second run
+	 */
+	public void testParityRules2() {
+
+		gen.nextGeneration();
+		// alive cells after first run
+		assertEquals(true, gen.getGrid().getCell(1, 1).isAlive());
+		assertEquals(true, gen.getGrid().getCell(0, 2).isAlive());
+		assertEquals(true, gen.getGrid().getCell(2, 2).isAlive());
+		assertEquals(true, gen.getGrid().getCell(1, 3).isAlive());
+		assertEquals(true, gen.getGrid().getCell(1, 0).isAlive());
+		assertEquals(true, gen.getGrid().getCell(3, 2).isAlive());
+		// dead cells after first run
+		assertEquals(false, gen.getGrid().getCell(0, 0).isAlive());
+		assertEquals(false, gen.getGrid().getCell(0, 1).isAlive());
+		assertEquals(false, gen.getGrid().getCell(0, 3).isAlive());
+		assertEquals(false, gen.getGrid().getCell(2, 0).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 0).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 1).isAlive());
+		assertEquals(false, gen.getGrid().getCell(3, 3).isAlive());
 	}
 
 }
