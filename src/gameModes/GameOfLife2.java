041f6a927c0314e578e6e5e693ab808675aa855e
Jan Keller <jan.keller@nordakademie.de>
2017-10-27 19:03:01 +0200

    GoL 1-3 Parity 1_1, 1_2, 2_1, 2_2

--- src/gameModes/GameOfLife2.java
+++ src/gameModes/GameOfLife2.java
@@ -1,12 +1,34 @@
 package gameModes;
 
+import game.Controller;
+
 public class GameOfLife2 {
+	static Controller controller = new Controller();
 
 	public static void main(String[] args) {
-		GameOfLifeConfig2 newGen = new GameOfLifeConfig2(100, "HashMap", 0, "Moore");
-		for (int i = 1; i <= 100; i++) {
-			newGen.nextGeneration();
-			newGen.drawGrid();
+		controller.createGame(100, "HashMap", 0, "Moore", 1);
+		setStartConfiguration();
+		controller.drawGrid();
+		controller.nextGeneration();
+	}
+
+	public static void setStartConfiguration() {
+		boolean lives;
+		for (int row = 0; row < controller.getGrid().getGridDimension() - 1; row++) {
+			if (row % 2 == 0) {
+				lives = false;
+			} else {
+				lives = true;
+			}
+			for (int col = 0; col < controller.getGrid().getGridDimension() - 1; col++) {
+				if (lives) {
+					controller.getGrid().getCell(row, col).live();
+				} else {
+					controller.getGrid().getCell(row, col).die();
+				}
+				lives = !lives;
+			}
 		}
+		controller.drawGrid();
 	}
 }
