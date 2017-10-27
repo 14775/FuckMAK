109864d3d1d9d82ab944f84162e13a9aa4718868
Jan Keller <jan.keller@nordakademie.de>
2017-10-27 19:03:46 +0200

    what the.. 1 more

---
+++ src/gameModes/Parity_Experiment_2_1.java
@@ -0,0 +1,34 @@
+package gameModes;
+
+import game.Controller;
+
+public class Parity_Experiment_2_1 {
+	static Controller controller = new Controller();
+
+	public static void main(String[] args) {
+		controller.createGame(200, "2DArray", 1, "VonNeumann", 2, 100);
+		setStartConfiguration();
+		controller.drawGrid();
+		controller.nextGeneration();
+	}
+
+	public static void setStartConfiguration() {
+		boolean lives;
+		for (int row = 0; row < controller.getGrid().getGridDimension() - 1; row++) {
+			if (row % 2 == 0) {
+				lives = false;
+			} else {
+				lives = true;
+			}
+			for (int col = 0; col < controller.getGrid().getGridDimension() - 1; col++) {
+				if (lives) {
+					controller.getGrid().getCell(row, col).live();
+				} else {
+					controller.getGrid().getCell(row, col).die();
+				}
+				lives = !lives;
+			}
+		}
+		controller.drawGrid();
+	}
+}
