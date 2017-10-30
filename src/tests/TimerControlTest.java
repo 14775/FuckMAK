9796ff6c5310c8b7181a8443968c742fd439e3a9
14710 <nico.knueppel@nordakademie.de>
2017-10-30 18:35:06 +0100

    added TimerControlTest and added millisecondsConverter

---
+++ src/tests/TimerControlTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+public class TimerControlTest {
+
+	/*
+	 * tests the time converter (milliseconds into the format hh:mm:ss)
+	 */
+	@Test
+	public void millisecondsConverterTest() {
+		final long millisecondsExample = 11567834;
+		String result = "";
+
+		result = timer.TimeControl.millisecondsConverter(millisecondsExample);
+
+		assertEquals(result, "03:12:47");
+		assertNotEquals(result, 11567834);
+
+	}
+
+}
