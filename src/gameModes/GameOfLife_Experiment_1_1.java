d8001e6f0814b5c43ea62e856908a53bf40f9f84
14592 <willi.prahl@nordakademie.de>
2017-11-02 21:44:48 +0100

    fixed startconfig 1

--- src/gameModes/GameOfLife_Experiment_1_1.java
+++ src/gameModes/GameOfLife_Experiment_1_1.java
@@ -16,28 +16,28 @@ public class GameOfLife_Experiment_1_1 {
 
 	private static void setStartConfiguration() {
 
+		controller.getGrid().getCell(17, 17).live();
+		controller.getGrid().getCell(17, 18).live();
+		controller.getGrid().getCell(17, 20).live();
+		controller.getGrid().getCell(17, 21).live();
+		controller.getGrid().getCell(18, 17).live();
 		controller.getGrid().getCell(18, 18).live();
-		controller.getGrid().getCell(18, 19).live();
+		controller.getGrid().getCell(18, 20).live();
 		controller.getGrid().getCell(18, 21).live();
-		controller.getGrid().getCell(18, 22).live();
 		controller.getGrid().getCell(19, 18).live();
-		controller.getGrid().getCell(19, 19).live();
-		controller.getGrid().getCell(19, 21).live();
-		controller.getGrid().getCell(19, 22).live();
-		controller.getGrid().getCell(20, 19).live();
-		controller.getGrid().getCell(20, 21).live();
-		controller.getGrid().getCell(21, 17).live();
-		controller.getGrid().getCell(21, 19).live();
-		controller.getGrid().getCell(21, 21).live();
-		controller.getGrid().getCell(21, 23).live();
+		controller.getGrid().getCell(19, 20).live();
+		controller.getGrid().getCell(20, 16).live();
+		controller.getGrid().getCell(20, 18).live();
+		controller.getGrid().getCell(20, 20).live();
+		controller.getGrid().getCell(20, 22).live();
+		controller.getGrid().getCell(21, 16).live();
+		controller.getGrid().getCell(21, 18).live();
+		controller.getGrid().getCell(21, 20).live();
+		controller.getGrid().getCell(21, 22).live();
+		controller.getGrid().getCell(22, 16).live();
 		controller.getGrid().getCell(22, 17).live();
-		controller.getGrid().getCell(22, 19).live();
 		controller.getGrid().getCell(22, 21).live();
-		controller.getGrid().getCell(22, 23).live();
-		controller.getGrid().getCell(23, 17).live();
-		controller.getGrid().getCell(23, 18).live();
-		controller.getGrid().getCell(23, 22).live();
-		controller.getGrid().getCell(23, 23).live();
+		controller.getGrid().getCell(22, 22).live();
 
 	}
 
